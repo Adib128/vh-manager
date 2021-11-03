fc6ddfd2f6152655dd3bbeed0196b87143e987b6
Adib <aouadi.adib@gmail.com>
2021-11-03 17:10:07 +0100

    Deleting history

--- .history/src/drivers/drivers.module_20211101170622.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DriversService } from './drivers.service';
-import { DriversController } from './drivers.controller';
-
-@Module({
-  controllers: [DriversController],
-  providers: [DriversService]
-})
-export class DriversModule {}
--- .history/src/fuels/fuels.module_20211102123107.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FuelsService } from './fuels.service';
-import { FuelsController } from './fuels.controller';
-
-@Module({
-  controllers: [FuelsController],
-  providers: [FuelsService]
-})
-export class FuelsModule {}
